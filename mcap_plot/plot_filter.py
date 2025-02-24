b36ae5fd5528012831e02e0f0a9e7df2615d187d
minhducquach <minhduc16102@gmail.com>
2025-02-24 13:55:24 +0100

    new week

--- mcap_plot/plot_filter.py
+++ mcap_plot/plot_filter.py
@@ -1,19 +1,38 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import least_squares
 
-file = open('/home/manip/ros2_ws/src/mcap_plot/mcap_plot/light_tab_dis_data.txt')
+# Load dataset
+data = np.loadtxt("/media/minhducquach/MiduT71/STUDY/SLAM/Internship/mcap_datareader/mcap_plot/light_tab_dis_data_38.txt")
+distance = data[:, 0]
+intensity = data[:, 1]
 
-point_x = []
-point_y = []
-flag = 0
+# Define the model function
+def model(params, d):
+    A, B, C = params
+    return A / (B + d**2) + C
 
-for line in file:
-    line_split = line.split()
-    point_x.append(float(line_split[0]))
-    point_y.append(int(line_split[1]))
+# Define residual function for least_squares
+def residuals(params, d, I):
+    return model(params, d) - I
 
-point_x = np.array(point_x)
-point_y = np.array(point_y)
+# Initial guess for A, B, C
+initial_guess = [1, 1, 1]
 
-plt.scatter(point_x, point_y)
+# Perform robust fitting using least_squares with Huber loss
+result = least_squares(residuals, initial_guess, loss='soft_l1', args=(distance, intensity))
+
+# Extract fitted parameters
+A_fit, B_fit, C_fit = result.x
+print(f"Estimated parameters (robust fitting): A={A_fit:.3f}, B={B_fit:.3f}, C={C_fit:.3f}")
+
+# Generate fitted values
+fitted_intensity = model(result.x, distance)
+
+# Plot results
+plt.scatter(distance, intensity, label="Data", color="blue", alpha=0.6)
+plt.plot(distance, fitted_intensity, color="red", label="Fitted Curve")
+plt.xlabel("Distance")
+plt.ylabel("Intensity")
+plt.legend()
 plt.show()
